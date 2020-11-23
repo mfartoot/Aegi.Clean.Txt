1bdb28d2b51925673a54028ffb42856db4e4c948
President <mfartoot14@gmail.com>
2020-11-24 01:28:47 +0330

    Update SubtitleCleaner.lua
    Bug fixes, social spacing, and change name.

--- SubtitleCleaner.lua
+++ SubtitleCleaner.lua
@@ -1,17 +1,24 @@
 -- :raven
 
-script_name = "Clean Text"
+script_name = "Aegi Clean Text"
 script_description = "Clean all selected text columns inside script and keap timings."
-script_version = "1.0"
+script_version = "1.0.1"
 script_author = ":raven"
 
 function clean_text(subtitles, selected_lines, active_line)
-	for z, i in ipairs(selected_lines) do
-		local sub = subtitles[i]
+	for i, j in ipairs(selected_lines) do
+		local sub = subtitles[j]
+		
 		-- if you remove blank character in (line 10) dialogs will be removed.
-		sub.text = ' '
-		subtitles[i] = sub
+		-- sub.text = ' '
+
+		-- in new release(1.0.1) the problam solved
+		sub.text = "\n\r"
+		sub.text = sub.text:gsub("\n", '')
+		
+		subtitles[j] = sub
 	end
+	
 	aegisub.set_undo_point(script_name)
 end
 
