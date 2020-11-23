42b9a099b87bd243d1f712529ea5c27a1d561ff9
President <mfartoot14@gmail.com>
2020-11-23 20:01:33 +0330

    Update SubtitleCleaner.lua

--- SubtitleCleaner.lua
+++ SubtitleCleaner.lua
@@ -1,4 +1,4 @@
-
+-- :raven
 
 script_name = "Clean Text"
 script_description = "Clean all selected text columns inside script and keap timings."
@@ -8,6 +8,7 @@ script_author = ":raven"
 function clean_text(subtitles, selected_lines, active_line)
 	for z, i in ipairs(selected_lines) do
 		local sub = subtitles[i]
+		-- if you remove blank character in (line 10) dialogs will be removed.
 		sub.text = ' '
 		subtitles[i] = sub
 	end
