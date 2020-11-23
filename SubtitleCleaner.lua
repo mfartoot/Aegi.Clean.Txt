28ccf157441ba12ffb38036ffebc4da9ae508f71
President <mfartoot14@gmail.com>
2020-11-23 19:21:32 +0330

    Add files via upload

---
+++ SubtitleCleaner.lua
@@ -0,0 +1,17 @@
+
+
+script_name = "Clean Text"
+script_description = "Clean all selected text columns inside script and keap timings."
+script_version = "1.0"
+script_author = ":raven"
+
+function clean_text(subtitles, selected_lines, active_line)
+	for z, i in ipairs(selected_lines) do
+		local sub = subtitles[i]
+		sub.text = ' '
+		subtitles[i] = sub
+	end
+	aegisub.set_undo_point(script_name)
+end
+
+aegisub.register_macro(script_name, "Clean all selected dialoges text columns.", clean_text)
